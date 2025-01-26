2b6c6858a8b77122e3bf3f6772b43a934cff9198
Ali <a3.nawaf@gmail.com>
2025-01-26 22:28:11 +0000

    Third variation, making sure user cannot input 0

--- euclidean1.py
+++ euclidean1.py
@@ -9,7 +9,10 @@ if __name__ == "__main__": #running the code with user inputs
     try:
         num1 = int(input("Enter the first positive integer: ")) #takes first input
         num2 = int(input("Enter the second positive integer: ")) #takes second input
-        gcd_result = euclideanalgorithm.get_gcd(num1, num2) #applies get_gcd function
-        print(f"The GCD of {num1} and {num2} is: {gcd_result}") #print result
-    except ValueError: #makes sure values are correct
+        if num1<=0 or num2<=0: #makes sure non of the numbers are 0 or less
+            raise ValueError("Both inputs must be positive integers")
+        else:
+            gcd_result = euclideanalgorithm.get_gcd(num1, num2) #applies get_gcd function
+            print(f"The GCD of {num1} and {num2} is: {gcd_result}") #print result
+    except ValueError: #makes sure that numbers are inputted
         print("Invalid input. Please ensure you enter positive integers.")
